e0acd326b9c0cb95c09139d29e27e26019918daa
musitive <dallinfrank@gmail.com>
2019-01-28 22:02:19 -0700

    Start Character Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        //fixedDeltaTime assures same speed no matter how frequent this function is called
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);// crouch and jump
+    }
+}
