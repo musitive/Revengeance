832f7354baf201ff00e80dc1a9b11a64fbd276d4
James Wasson <jameswasson@users.noreply.github.com>
2019-04-14 00:09:26 -0600

    Score System Implemented
    Killing a bad guy gives 200 poinsts btw.

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    static int currentScore = 0;
+    public Text scoreDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string displayText = "Score: " + currentScore.ToString("n0");
+        scoreDisplay.text = displayText;
+    }
+
+    public static void gainPoints(int points) {
+        currentScore += points;
+    }
+}
